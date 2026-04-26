49a22cf763cd18ed95037ddda71f2e6e19595b83
unknown <COMPLAB1@LAB1-26.ASIANCOLLEGE.LOCAL>
2026-04-26 17:31:00 +0800

    first commit

---
+++ app/index.tsx
@@ -0,0 +1,36 @@
+import { Text, StyleSheet, ScrollView } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+
+export default function Index() {
+  const name: string = "Tepacia Ross"
+  return (
+    <SafeAreaView
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <ScrollView>
+        <Text style={styles.Text1}>Welcome, {name}</Text>
+        <Text style={styles.Text2}>Hello World! Expo</Text>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+
+const styles = StyleSheet.create({
+  Text1: {
+    color: 'red',
+    fontSize: 40
+  },
+  Text2: {
+    backgroundColor: 'red',
+    color: 'white',
+    fontSize: 50
+  }
+});
+
+
